0d720a7d9a856290c6881e6ed66ece0bd9fc33a9
Timothy Danford <tdanford@gmail.com>
2010-10-25 15:45:32 -0400

    Initial check-in of AlzForum package Java code.

---
+++ packages/alzforum/java/src/main/org/sc/antibodies/AntibodyLoader.java
@@ -0,0 +1,15 @@
+package org.sc.antibodies;
+
+import java.util.*;
+import java.io.*;
+
+public class AntibodyLoader {
+	
+	public static void main(String[] args) throws IOException { 
+		new AntibodyLoader(new File(args[0]));
+	}
+
+	public AntibodyLoader(File f) throws IOException { 
+		
+	}
+}
