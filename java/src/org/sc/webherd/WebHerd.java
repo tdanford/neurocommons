1b41a5f62145d889af3f5c1fac849d41d060ab63
Timothy Danford <tdanford@virtualbox.(none)>
2010-11-26 09:20:14 -0500

    Added main() method.

--- java/src/org/sc/webherd/WebHerd.java
+++ java/src/org/sc/webherd/WebHerd.java
@@ -5,4 +5,10 @@ import java.io.*;
 
 public class WebHerd {
 
+    public static void main(String[] args) { 
+	String bundleDir = args[0];
+	String resourceBase = args.length > 1 ? args[1] : ".";
+	int port = 30301;
+	WebHerdJettyServer server = new WebHerdJettyServer(port, resourceBase);
+    }
 }
